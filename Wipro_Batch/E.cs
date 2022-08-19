4621375594679de514fbb5b35a6c4c493e490f77
akashshelar96 <akashshelar1155@outlook.com>
2022-08-19 20:42:52 +0530

    Add project files.

---
+++ Wipro_Batch/E.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wipro_Batch
+{
+    class E
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Wipro_Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wipro_Batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
